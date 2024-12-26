186e2acb097723e83c6ab0c7245fc547ad5b45f9
Minh Le <beaver700nh@gmail.com>
2024-12-26 13:53:46 -0500

    Initial commit

---
+++ default/user.js
@@ -0,0 +1,54 @@
+user_pref(
+  "devtools.toolbox.host",
+  "right"
+);
+
+user_pref(
+  "browser.download.useDownloadDir",
+  false
+);
+
+user_pref(
+  "browser.toolbars.bookmarks.visibility",
+  "always"
+);
+
+user_pref(
+  "browser.urlbar.showSearchSuggestionsFirst",
+  false
+);
+
+user_pref(
+  "browser.urlbar.suggest.bookmark",
+  false
+);
+
+user_pref(
+  "browser.urlbar.suggest.openpage",
+  false
+);
+
+user_pref(
+  "browser.urlbar.suggest.topsites",
+  false
+);
+
+user_pref(
+  "browser.urlbar.suggest.engines",
+  false
+);
+
+user_pref(
+  "privacy.donottrackheader.enabled",
+  true
+);
+
+user_pref(
+  "privacy.globalprivacycontrol.enabled",
+  true
+);
+
+user_pref(
+  "browser.uiCustomization.state",
+  '{ "placements": { "widget-overflow-fixed-list": [], "unified-extensions-area": [], "nav-bar": [ "back-button", "forward-button", "stop-reload-button", "home-button", "urlbar-container", "downloads-button", "developer-button", "fxa-toolbar-menu-button", "unified-extensions-button" ], "toolbar-menubar": [ "menubar-items" ], "TabsToolbar": [ "firefox-view-button", "tabbrowser-tabs", "new-tab-button", "alltabs-button" ], "PersonalToolbar": [ "personal-bookmarks" ] }, "seen": [ "save-to-pocket-button", "developer-button", "profiler-button" ], "dirtyAreaCache": [ "nav-bar", "PersonalToolbar", "toolbar-menubar", "TabsToolbar" ], "currentVersion": 20, "newElementCount": 3 }'
+);
---
+++ school/user.js
@@ -0,0 +1,59 @@
+user_pref(
+  "devtools.toolbox.host",
+  "right"
+);
+
+user_pref(
+  "browser.download.useDownloadDir",
+  false
+);
+
+user_pref(
+  "browser.toolbars.bookmarks.visibility",
+  "always"
+);
+
+user_pref(
+  "browser.urlbar.showSearchSuggestionsFirst",
+  false
+);
+
+user_pref(
+  "browser.urlbar.suggest.bookmark",
+  false
+);
+
+user_pref(
+  "browser.urlbar.suggest.openpage",
+  false
+);
+
+user_pref(
+  "browser.urlbar.suggest.topsites",
+  false
+);
+
+user_pref(
+  "browser.urlbar.suggest.engines",
+  false
+);
+
+user_pref(
+  "privacy.donottrackheader.enabled",
+  true
+);
+
+user_pref(
+  "privacy.globalprivacycontrol.enabled",
+  true
+);
+
+user_pref(
+  "browser.uiCustomization.state",
+  '{ "placements": { "widget-overflow-fixed-list": [], "unified-extensions-area": [], "nav-bar": [ "back-button", "forward-button", "stop-reload-button", "home-button", "urlbar-container", "downloads-button", "developer-button", "fxa-toolbar-menu-button", "unified-extensions-button" ], "toolbar-menubar": [ "menubar-items" ], "TabsToolbar": [ "firefox-view-button", "tabbrowser-tabs", "new-tab-button", "alltabs-button" ], "PersonalToolbar": [ "personal-bookmarks" ] }, "seen": [ "save-to-pocket-button", "developer-button", "profiler-button" ], "dirtyAreaCache": [ "nav-bar", "PersonalToolbar", "toolbar-menubar", "TabsToolbar" ], "currentVersion": 20, "newElementCount": 3 }'
+);
+
+user_pref(
+  "browser.newtabpage.pinned",
+  '[ { "url": "https://holliston.powerschool.com/guardian/home.html?_userTypeHint=student", "label": "PS" }, { "url": "https://classroom.google.com/", "label": "Classroom" }, { "url": "https://mail.google.com/mail/u/0/#inbox", "label": "Mail" }, { "url": "https://drive.google.com/drive/my-drive", "label": "Drive" }, { "url": "https://docs.google.com/document/u/0/?tgif=d", "label": "Docs" }, { "url": "https://docs.google.com/spreadsheets/u/0/?tgif=d", "label": "Sheets" }, { "url": "https://docs.google.com/presentation/u/0/?tgif=c", "label": "Slides" }, { "url": "https://calendar.google.com/calendar/u/0/r?pli=1", "label": "Calendar" } ]'
+);
